e3f5dfcb8cb4d14bc5e6948855bef700db61cc63
Luis1f2 <221271@ids.upchiapas.edu.mx>
2026-07-15 03:02:28 -0600

    cambios para la ejecucion

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,10 +41,11 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
-
+}
     defaultConfig {
         applicationId = "com.explorachiapas.app"
         minSdk = flutter.minSdkVersion
